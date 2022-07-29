17e7f1375744cd378b62b8dd105d746ff70a2053
Kamyab Rostami <kamyabrostamii@gmail.com>
2022-07-29 05:41:20 +0430

    Refactoring

--- TaxCalculator.Test/IndividualTaxCalculatorTest.cs
+++ TaxCalculator.Test/IndividualTaxCalculatorTest.cs
@@ -1,79 +1,74 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TaxCalculator.Pay;
+﻿using TaxCalculator.Pay;
 using TaxCalculator.Pay.Entities;
+using Xunit;
 
+namespace TaxCalculator.Test;
 
-namespace TaxCalculator.Test
+public class IndividualTaxCalculatorTest
 {
-    public class IndividualTaxCalculatorTest
+    [Fact]
+    public void Non_TaxtResident_Should_Not_Calculated()
     {
-        [Fact]
-        public void Non_TaxtResident_Should_Not_Calculated()
+        //Arrange
+        TaxPayer taxPayer = new TaxPayer
         {
-            //Arrange
-            TaxPayer taxPayer = new TaxPayer
-            {
-                IsCitizen = false,
-            };
-            IndividualTaxCalculator individualTax = new IndividualTaxCalculator();
+            IsCitizen = false,
+        };
+        IndividualTaxCalculator individualTax = new IndividualTaxCalculator();
 
-            //Act
+        //Act
 
-            string expectedErrorMessage = "Person is not citizen";
-            var exceptionResult = Assert.Throws<InvalidOperationException>(() =>
-            {
-                individualTax.CalculationTaxPercentage(taxPayer);
-            });
-
-            //Assert
-
-            Assert.Equal(expectedErrorMessage, exceptionResult.Message);
-        }
-
-
-        [Fact]
-        public void Diaable_Taxtpayer_Should_Pay_Zero_Percent_Tax()
+        string expectedErrorMessage = "Person is not citizen";
+        var exceptionResult = Assert.Throws<InvalidOperationException>(() =>
         {
-            //Arrange
-            TaxPayer taxPayer = new TaxPayer
-            {
-                IsCitizen = true,
-                HasDisability = true
-            };
-            IndividualTaxCalculator individualTax = new IndividualTaxCalculator();
+            individualTax.CalculationTaxPercentage(taxPayer);
+        });
 
-            //Act
-            var result = individualTax.CalculationTaxPercentage(taxPayer);
+        //Assert
 
-            //Assert
+        Assert.Equal(expectedErrorMessage, exceptionResult.Message);
+    }
 
-            Assert.Equal(0, result);
-        }
 
-        //private TaxCalculator _taxCalculator = new TaxCalculator();
-
-        [Fact]
-        public void Retired_Taxpayer_Should_Pay_One_Percent_Tax()
+    [Fact]
+    public void Diaable_Taxtpayer_Should_Pay_Zero_Percent_Tax()
+    {
+        //Arrange
+        TaxPayer taxPayer = new TaxPayer
         {
-            //Arrange
-            
-            TaxPayer taxPayer = new TaxPayer
-            {
-                IsCitizen = true,
-                HasDisability = true
-            };
-            IndividualTaxCalculator individualTax = new IndividualTaxCalculator();
+            IsCitizen = true,
+            HasDisability = true
+        };
+        IndividualTaxCalculator individualTax = new IndividualTaxCalculator();
 
-            //Act
-            var result = individualTax.CalculationTaxPercentage(taxPayer);
+        //Act
+        var result = individualTax.CalculationTaxPercentage(taxPayer);
 
-            //Assert
+        //Assert
 
-            Assert.Equal(0, result);
-        }
+        Assert.Equal(0, result);
+    }
+
+    //private TaxCalculator _taxCalculator = new TaxCalculator();
+
+    [Fact]
+    public void Retired_Taxpayer_Should_Pay_One_Percent_Tax()
+    {
+        //Arrange
+        
+        TaxPayer taxPayer = new TaxPayer
+        {
+            IsCitizen = true,
+            HasDisability = true
+        };
+        IndividualTaxCalculator individualTax = new IndividualTaxCalculator();
+
+        //Act
+        var result = individualTax.CalculationTaxPercentage(taxPayer);
+
+        //Assert
+
+        Assert.Equal(0, result);
     }
 }
+
